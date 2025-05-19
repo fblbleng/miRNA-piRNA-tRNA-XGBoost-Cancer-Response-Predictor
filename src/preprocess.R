687d1fdbd582d48437e98ac197af6bad18ad529f
Fabiola <fblbleng@users.noreply.github.com>
2025-05-19 18:17:17 +0200

    Create preprocess.R

---
+++ src/preprocess.R
@@ -0,0 +1,15 @@
+---
+title: "RNA Data Preprocessing Pipeline"
+author: "Fabiola Blengio"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+library(dplyr)
+library(readr)
+library(scales)
+library(tibble)
+```
+
+# 1. Load Datasets
